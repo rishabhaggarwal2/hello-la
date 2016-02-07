5bbe2e02de026a2c3e0b79bd01b7c594d44cd083
Evan Derby <somekidwithhtml@gmail.com>
2016-02-07 00:00:14 -0500

    Added header images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,14 +12,32 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
 
 var ready;
 ready = function() {
-  $("#category").change(function(e) {
-    window.location.pathname = "/category/" + $(this).val().toLowerCase();
-  })
+  var background_image_url = $('.floating-name').attr("title");
+  $('.floating-name').css('background-image','url(../assets/' + background_image_url + ')');
+
+  var controller = new ScrollMagic.Controller({
+		globalSceneOptions: {
+			triggerHook: 'onLeave'
+		}
+	});
+
+	// get all slides
+	var slides = document.querySelectorAll("section");
+
+	// create scene for every slide
+	for (var i=0; i<slides.length; i++) {
+		new ScrollMagic.Scene({
+				triggerElement: slides[i]
+			})
+			.setPin(slides[i])
+			.addTo(controller);
+	}
 }
 
 $(document).ready(ready);
